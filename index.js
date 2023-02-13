54bbb786e49ac760af0513ce34d9bf6ea119fa5c
aepc5 <67446891+aepc5@users.noreply.github.com>
2023-02-13 16:00:49 -0500

    Finished tutorial

--- index.js
+++ index.js
@@ -1,21 +1,75 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+
+//models
+const TodoTask = require("./models/TodoTask");
+
+dotenv.config();
 
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
+
+//Connect to the MongoDB database
+mongoose.connect(process.env.DB_CONNECT).then(()=>{
+    console.log("Connected to db!");
+
+    app.listen(3000, () => console.log("Server Up and running"));
+})
+
 // View Engine configuration
 app.set("view engine", "ejs");
 
-// Get method
-app.get('/',(req, res) => {
-    res.render('todo.ejs');
+// GET METHOD
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todo.ejs", { todoTasks: tasks });
+    });
 });
 
-// Post Method
-app.post('/',(req, res) => {
-    console.log(req.body);
+//POST METHOD
+app.post('/',async (req, res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
 });
 
-app.listen(3000, () => console.log("Server Up and running"));
+// Update todo item
+app
+    .route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id }); // Render the edit template
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id; // Save the id from the request
+
+        // Call function to update task with the id
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/"); // Return to home
+        });
+    });
+
+// Delete todo item
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id; // Save the id from the request
+
+    // Call function to remove task with the id
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/"); // Return to home
+    });
+});
+
